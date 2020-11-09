4a4fa83f0ff73490188f519857dcbcb94d6043d5
RidhoAja <ridhoakbarsyah15@gmail.com>
2020-11-09 21:32:11 +0700

    Praktikum2

---
+++ Praktikum 2/app/src/main/java/com/ridho_18104020/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ridho_18104020.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
