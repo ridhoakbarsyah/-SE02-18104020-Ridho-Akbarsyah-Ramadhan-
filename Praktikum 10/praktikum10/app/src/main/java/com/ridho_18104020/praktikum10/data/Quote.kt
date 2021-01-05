29d64b74757e17da8bab9c5b563b713c3f4eba07
Ridho Akbarsyah <you@example.com>
2021-01-05 16:56:29 +0700

    Apps Praktikum10

---
+++ Praktikum 10/praktikum10/app/src/main/java/com/ridho_18104020/praktikum10/data/Quote.kt
@@ -0,0 +1,13 @@
+package com.ridho_18104020.praktikum10.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
